0cb4186a929115133cefce93224de11c140c3b96
Mai Thi Phan <phanthimainct0102@gmail.com>
2025-06-13 15:40:35 +0700

    Initial app routes

---
+++ finance/lib/app/router/app_routes.dart
@@ -0,0 +1,18 @@
+class AppRoutes {
+  // Main navigation routes
+  static const String home = '/';
+  static const String transactions = '/transactions';
+  static const String budgets = '/budgets';
+  static const String more = '/more';
+  
+  // Secondary routes
+  static const String settings = '/settings';
+  
+  // Additional pages that can be customized in nav bar
+  static const String goals = '/goals';
+  static const String analytics = '/analytics';
+  static const String profile = '/profile';
+  static const String notifications = '/notifications';
+  static const String help = '/help';
+  static const String about = '/about';
+} 
