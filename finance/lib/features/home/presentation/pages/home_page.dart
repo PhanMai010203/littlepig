c17bea9cd84791ab60eb371dc5d70e7b8000d78c
Mai Thi Phan <phanthimainct0102@gmail.com>
2025-06-22 16:11:12 +0700

    Reorganize

--- finance/lib/features/home/presentation/pages/home_page.dart
+++ finance/lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:finance/shared/widgets/page_template.dart';
-import 'package:marquee/marquee.dart';
+import '../../widgets/account_card.dart';
 import '../../widgets/home_page_username.dart';
 
 class HomePage extends StatefulWidget {
@@ -66,8 +66,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                     clipBehavior: Clip.none,
                     child: Row(
                       children: [
-                        _AccountCard(
-                          title: 'Ngân hàngggggggggggggg',
+                        AccountCard(
+                          title: 'N gân hàngggggggggggggg',
                           amount: 'đ530.000 VND',
                           transactions: '260 transactions',
                           color: const Color(0xFF439A97),
@@ -79,7 +79,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                           },
                         ),
                         const SizedBox(width: 16),
-                        _AccountCard(
+                        AccountCard(
                           title: 'Tín dụng',
                           amount: 'đ530.000 VND',
                           transactions: '260 transactions',
@@ -92,7 +92,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                           },
                         ),
                         const SizedBox(width: 16),
-                        _AddAccountCard(
+                        AddAccountCard(
                           onTap: () {},
                         ),
                       ],
@@ -104,170 +104,6 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           ),
         ),
       ],
-    );
-  }
-}
-
-class _AccountCard extends StatelessWidget {
-  final String title;
-  final String amount;
-  final String transactions;
-  final Color color;
-  final bool isSelected;
-  final VoidCallback onTap;
-
-  const _AccountCard({
-    required this.title,
-    required this.amount,
-    required this.transactions,
-    required this.color,
-    required this.isSelected,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        width: 170,
-        height: 110,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: isSelected ? color : Colors.transparent,
-            width: 2,
-            strokeAlign: BorderSide.strokeAlignInside,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 20,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 20,
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        const style = const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        );
-                        final textPainter = TextPainter(
-                          text: TextSpan(text: title, style: style),
-                          maxLines: 1,
-                          textDirection: TextDirection.ltr,
-                        )..layout(maxWidth: constraints.maxWidth);
-
-                        if (textPainter.didExceedMaxLines) {
-                          return Marquee(
-                            text: title,
-                            style: style,
-                            scrollAxis: Axis.horizontal,
-                            blankSpace: 20.0,
-                            velocity: 30.0,
-                            pauseAfterRound: const Duration(seconds: 2),
-                            fadingEdgeEndFraction: 0.1,
-                            fadingEdgeStartFraction: 0.1,
-                          );
-                        } else {
-                          return Text(title, style: style);
-                        }
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Container(
-                  width: 18,
-                  height: 18,
-                  decoration: BoxDecoration(
-                    color: color,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ],
-            ),
-            const Spacer(),
-            Text(
-              amount,
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              transactions,
-              style: const TextStyle(
-                fontSize: 12,
-                color: Color(0xFF6A6A6A),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _AddAccountCard extends StatelessWidget {
-  final VoidCallback onTap;
-
-  const _AddAccountCard({required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 120,
-        height: 120,
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: const Color(0xFF6A6A6A).withOpacity(0.7),
-            width: 2,
-            strokeAlign: BorderSide.strokeAlignInside,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 20,
-              spreadRadius: 2,
-            )
-          ],
-        ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.add,
-              size: 28,
-              color: Color(0xFF6A6A6A),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Account',
-              style: TextStyle(
-                fontSize: 14,
-                color: Color(0xFF6A6A6A),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
