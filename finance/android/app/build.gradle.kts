83157956551db11e8e07b65eefde3080e0276492
Mai Thi Phan <phanthimainct0102@gmail.com>
2025-06-23 01:16:47 +0700

    Update version code and package name

--- finance/android/app/build.gradle.kts
+++ finance/android/app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.finance"
+    namespace = "com.sheep.finance"
+    testNameSpace = "com.sheep.finance.test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.finance"
+        applicationId = "com.sheep.finance"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
